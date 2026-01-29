37c7356afcc4b462b2b21a61cfcd8290e3f8d091
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-01-29 18:48:38 -0300

    exercício 0 de lógica de programação

--- src/logicadeprogramacao/Ex000.java
+++ src/logicadeprogramacao/Ex000.java
@@ -1,7 +1,23 @@
 package logicadeprogramacao;
-
+import java.util.Scanner;
 public class Ex000 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        /*Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
+        mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
+        (Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)*/
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a sua idade em anos: ");
+        int anos = scanner.nextInt();
+        System.out.println("Digite a quantidade de meses: ");
+        int meses = scanner.nextInt();
+        System.out.println("Digite a quantidade de dias: ");
+        int dias = scanner.nextInt();
+
+        double total = ((anos * 365) + (meses * 30) + dias);
+        System.out.println("Sua idade expressa em dias é: "+total+ " dias");
+
+        scanner.close();
     }
 }
